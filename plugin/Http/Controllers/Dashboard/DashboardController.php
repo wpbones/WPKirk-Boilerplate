df3d7becb25e11d22f3a78ff6b08e75c32687c12
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-09-23 15:29:01 +0200

    🚧

--- plugin/Http/Controllers/Dashboard/DashboardController.php
+++ plugin/Http/Controllers/Dashboard/DashboardController.php
@@ -5,13 +5,13 @@ namespace WPKirk\Http\Controllers\Dashboard;
 use WPKirk\Http\Controllers\Controller;
 
 if (!defined('ABSPATH')) {
-    exit();
+  exit();
 }
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        return WPKirk()->view('dashboard.index')->withAdminAppsScripts('app');
-    }
+  public function index()
+  {
+    return WPKirk()->view('dashboard.index')->withAdminAppsScript('app');
+  }
 }
