bb235b7aaf9095f0a3b13d38a22d4f18354fe7f7
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-11-10 14:25:11 +0100

    🔖 Bump release v1.10.0

--- wp-kirk.php
+++ wp-kirk.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Kirk Boilerplate
  * Plugin URI: https://github.com/wpbones/WPKirk-Boilerplate
  * Description: WP Bones Boilerplate WordPress plugin
- * Version: 1.9.7
+ * Version: 1.10.0
  * Requires at least: 6.2
  * Requires PHP: 7.4
  * Author: Giovambattista Fazioli
