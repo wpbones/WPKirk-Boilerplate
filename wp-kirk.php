129da4964efe343a1362395f08e2173d4b449b3d
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-09-23 17:18:07 +0200

    ⬆️ chore(release): bump WPBones to v2.0.8 and align plugin version
    - wpbones/wpbones constraint → ^2.0.8
    - symfony/yaml updated to clear the advisories reported by composer audit
    - version aligned to 2.0.8 in readme.txt, wp-kirk.php and package.json

--- wp-kirk.php
+++ wp-kirk.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Kirk Boilerplate
  * Plugin URI: https://github.com/wpbones/WPKirk-Boilerplate
  * Description: WP Bones Boilerplate WordPress plugin
- * Version: 2.0.7
+ * Version: 2.0.8
  * Requires at least: 6.6
  * Requires PHP: 8.1
  * Author: Giovambattista Fazioli
